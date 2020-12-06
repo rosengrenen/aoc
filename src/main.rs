04241e7474d2f3aa1cafb8aed1461e0e88849d55
Rasmus Rosengren <rasmus.rosengren@pm.me>
2020-12-06 09:09:55 +0100

    Allow for part selection

--- src/main.rs
+++ src/main.rs
@@ -44,16 +44,34 @@ fn main() {
 		panic!("Must input number as first argument");
 	}
 	let day: u32 = args[1].parse().expect("Not a number");
+	let part = if args.len() >= 3 {
+		args[2]
+			.parse::<i64>()
+			.map_or_else(|_| None, |part| Some(part))
+	} else {
+		None
+	};
 	let (solver, file) = get_solver(day);
-	let part1_now = Instant::now();
-	let part1_answer = solver.solve(&read_lines(&file), false);
-	let part1_time = part1_now.elapsed().as_secs_f32();
-	println!("Day {} (part 1) answer: {}", day, part1_answer);
-	println!("Day {} (part 1) time: {}", day, part1_time);
-	let part2_now = Instant::now();
-	let part2_answer = solver.solve(&read_lines(&file), true);
-	let part2_time = part2_now.elapsed().as_secs_f32();
-	println!("Day {} (part 2) answer: {}", day, part2_answer);
-	println!("Day {} (part 2) time: {}", day, part2_time);
-	println!("Day {} total time: {}", day, part1_time + part2_time);
+	let mut total_time = 0.0;
+	if part.is_none() || part.unwrap() == 1 {
+		let part1_now = Instant::now();
+		let part1_answer = solver.solve(&read_lines(&file), false);
+		let part1_time = part1_now.elapsed().as_secs_f32();
+		println!("Day {} (part 1) answer: {}", day, part1_answer);
+		println!("Day {} (part 1) time: {}", day, part1_time);
+		total_time += part1_time;
+	}
+
+	if part.is_none() || part.unwrap() == 2 {
+		let part2_now = Instant::now();
+		let part2_answer = solver.solve(&read_lines(&file), true);
+		let part2_time = part2_now.elapsed().as_secs_f32();
+		println!("Day {} (part 2) answer: {}", day, part2_answer);
+		println!("Day {} (part 2) time: {}", day, part2_time);
+		total_time += part2_time;
+	}
+
+	if part.is_none() {
+		println!("Day {} total time: {}", day, total_time);
+	}
 }
