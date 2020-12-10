e1f34bbe1114a292ca72a04065b93b39ac9e90f9
Rasmus Rosengren <rasmus.rosengren@pm.me>
2020-12-10 09:03:51 +0100

    update solution to not recurse

--- 2020/rust/src/day10/mod.rs
+++ 2020/rust/src/day10/mod.rs
@@ -1,37 +1,45 @@
 use crate::lib::Solver;
-use std::collections::HashSet;
 
 pub struct Day10Solver;
 
 impl Solver for Day10Solver {
 	fn solve_part_one(&self, input: &str) -> i64 {
 		let mut output_joltages = parse_output_joltages(input);
-		output_joltages.sort();
-		let mut ones = 1;
-		let mut threes = 1;
-		let mut prev_output_joltage = output_joltages[0];
-		for &output_joltage in &output_joltages[1..] {
-			match output_joltage - prev_output_joltage {
-				1 => ones += 1,
-				3 => threes += 1,
-				_ => (),
-			}
-
-			prev_output_joltage = output_joltage;
-		}
-
-		ones * threes
+		output_joltages.push(0);
+		output_joltages.sort_unstable();
+		solve_part_one(&output_joltages)
 	}
 
 	fn solve_part_two(&self, input: &str) -> i64 {
 		let mut output_joltages = parse_output_joltages(input);
 		output_joltages.push(0);
-		output_joltages.sort();
-		chunk_output_joltages(&output_joltages)
-			.iter()
-			.map(|part| permutations_in_part(part))
-			.product()
+		output_joltages.sort_unstable();
+		solve_part_two(&chunk_output_joltages(&output_joltages))
 	}
+}
+
+fn solve_part_one(output_joltages: &Vec<i64>) -> i64 {
+	let mut ones = 0;
+	let mut threes = 1;
+	let mut prev_output_joltage = output_joltages[0];
+	for &output_joltage in &output_joltages[1..] {
+		match output_joltage - prev_output_joltage {
+			1 => ones += 1,
+			3 => threes += 1,
+			_ => (),
+		}
+
+		prev_output_joltage = output_joltage;
+	}
+
+	ones * threes
+}
+
+fn solve_part_two(chunked_output_joltages: &Vec<Vec<i64>>) -> i64 {
+	chunked_output_joltages
+		.iter()
+		.map(|chunk| permutations_in_chunk(chunk))
+		.product()
 }
 
 fn parse_output_joltages(input: &str) -> Vec<i64> {
@@ -56,48 +64,54 @@ fn chunk_output_joltages(output_joltages: &Vec<i64>) -> Vec<Vec<i64>> {
 	chunks
 }
 
-fn permutations_in_part(part: &Vec<i64>) -> i64 {
+fn permutations_in_chunk(part: &Vec<i64>) -> i64 {
 	if part.len() < 3 {
 		return 1;
 	}
-	let mut permutations = HashSet::new();
-	permutations.insert(part.clone());
-	for i in 1..part.len() - 1 {
-		let mut part = part.clone();
-		part.remove(i);
-		if is_valid(&part) {
-			permutations.insert(part.clone());
-			permutations_in_part_recurse_search(part, &mut permutations);
-		}
-	}
-	permutations.len() as i64
-}
 
-fn permutations_in_part_recurse_search(part: Vec<i64>, permutations_acc: &mut HashSet<Vec<i64>>) {
-	if part.len() < 3 {
-		permutations_acc.insert(part.clone());
-		return;
-	}
-	for i in 1..part.len() - 1 {
-		let mut part = part.clone();
-		part.remove(i);
-		if is_valid(&part) {
-			permutations_acc.insert(part.clone());
-			permutations_in_part_recurse_search(part, permutations_acc);
-		}
-	}
-}
-
-fn is_valid(slice: &[i64]) -> bool {
-	let mut prev = slice[0];
-	for &part in slice.iter().skip(1) {
-		if part - prev > 3 {
-			return false;
+	let part_length = part.len();
+	let mut permutations = 1;
+	let mut index_stack = Vec::with_capacity(64);
+	index_stack.push(1);
+	loop {
+		if index_stack[0] == part_length - 1 {
+			break;
 		}
 
-		prev = part;
+		// Unwind stack
+		while index_stack[index_stack.len() - 1] > part_length - 1 {
+			index_stack.pop();
+		}
+
+		// Check if permutation is ok
+		let mut prev_joltage = part[0];
+		let mut valid = true;
+		for i in 1..part_length {
+			if index_stack.contains(&i) {
+				continue;
+			}
+
+			let current_joltage = part[i];
+			if current_joltage - prev_joltage > 3 {
+				valid = false;
+				break;
+			}
+
+			prev_joltage = current_joltage;
+		}
+
+		if valid {
+			permutations += 1;
+			let index_stack_length = index_stack.len();
+			index_stack[index_stack_length - 1] += 1;
+			index_stack.push(index_stack[index_stack.len() - 1] + 1);
+		} else {
+			let index_stack_length = index_stack.len();
+			index_stack[index_stack_length - 1] += 1;
+		}
 	}
-	true
+
+	permutations
 }
 
 #[cfg(test)]
@@ -124,13 +138,6 @@ mod tests {
 	}
 
 	#[bench]
-	fn bench_part_one(bencher: &mut Bencher) {
-		let input = include_str!("input.txt");
-		let solver = Day10Solver {};
-		bencher.iter(|| solver.solve_part_one(input));
-	}
-
-	#[bench]
 	fn bench_parse_output_joltages(bencher: &mut Bencher) {
 		let input = include_str!("input.txt");
 		bencher.iter(|| parse_output_joltages(input));
@@ -144,9 +151,31 @@ mod tests {
 	}
 
 	#[bench]
+	fn bench_part_one(bencher: &mut Bencher) {
+		let input = include_str!("input.txt");
+		let solver = Day10Solver {};
+		bencher.iter(|| solver.solve_part_one(input));
+	}
+
+	#[bench]
+	fn bench_part_one_skip_parse(bencher: &mut Bencher) {
+		let input = include_str!("input.txt");
+		let output_joltages = parse_output_joltages(input);
+		bencher.iter(|| solve_part_one(&output_joltages));
+	}
+
+	#[bench]
 	fn bench_part_two(bencher: &mut Bencher) {
 		let input = include_str!("input.txt");
 		let solver = Day10Solver {};
 		bencher.iter(|| solver.solve_part_two(input));
+	}
+
+	#[bench]
+	fn bench_part_two_skip_parse(bencher: &mut Bencher) {
+		let input = include_str!("input.txt");
+		let output_joltages = parse_output_joltages(input);
+		let chunked_output_voltages = chunk_output_joltages(&output_joltages);
+		bencher.iter(|| solve_part_two(&chunked_output_voltages));
 	}
 }
