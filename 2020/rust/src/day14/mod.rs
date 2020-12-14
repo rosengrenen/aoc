10f86941838fea69db59d1e30f7c7ef805f0e362
Rasmus Rosengren <rasmus.rosengren@pm.me>
2020-12-14 08:14:31 +0100

    Day 14 cleanup

--- 2020/rust/src/day14/mod.rs
+++ 2020/rust/src/day14/mod.rs
@@ -1,20 +1,21 @@
 use crate::lib::Solver;
-use itertools::Itertools;
 use std::collections::HashMap;
 
 pub struct Day14Solver;
 
 impl Solver for Day14Solver {
 	fn solve_part_one(&self, input: &str) -> i64 {
-		let instructions = parse(input);
-		let mut mask = (0, 0);
+		let instructions = instructions(input);
+		let mut mask = SetMaskArgs::default();
 		let mut mem: HashMap<i64, i64> = HashMap::new();
 		for instruction in instructions.iter() {
 			match instruction {
-				Instruction::Mask(mask_bits, override_bits) => mask = (*mask_bits, *override_bits),
-				Instruction::Mem(address, value) => {
-					let result = (value & !mask.0) | (mask.0 & mask.1);
-					mem.insert(*address, result);
+				&Instruction::SetMask(mask_args) => mask = mask_args,
+				&Instruction::Write(WriteArgs { address, value }) => {
+					// First part clears bits that are set by mask and second part
+					// sets mask bits
+					let value = (value & !mask.mask_bits) | mask.override_bits;
+					mem.insert(address, value);
 				}
 			}
 		}
@@ -22,42 +23,43 @@ impl Solver for Day14Solver {
 	}
 
 	fn solve_part_two(&self, input: &str) -> i64 {
-		let instructions = parse(input);
-		let mut mask = (0, 0);
+		let instructions = instructions(input);
+		let mut mask = SetMaskArgs::default();
 		let mut mem: HashMap<i64, i64> = HashMap::new();
 		for instruction in instructions.iter() {
 			match instruction {
-				Instruction::Mask(mask_bits, override_bits) => mask = (*mask_bits, *override_bits),
-				Instruction::Mem(address, value) => {
-					// println!("orig {:b}, {}", address, address);
-					// println!("mask {:b}, {:b}", mask.0, mask.1);
-					// println!("yes {:b}, {:b}", (address & mask.0), (mask.0 & mask.1));
-					let address = (address & mask.0) | (mask.0 & mask.1);
+				&Instruction::SetMask(mask_args) => mask = mask_args,
+				&Instruction::Write(WriteArgs { address, value }) => {
+					// The first part only save only masked bits, since the others are
+					// and need to be initialised to 0's
+					// The second part overwrites 0's with 1's if bitmask is 1
+					let address =
+						(address & mask.mask_bits) | (mask.mask_bits & mask.override_bits);
 
+					// Indices of floating bits
 					let mut floating_bit_indices = Vec::new();
 					for i in 0..36 {
-						if (!mask.0) & 1 << i != 0 {
+						// Inverting mask_bits gets floating bits
+						if (!mask.mask_bits) & 1 << i != 0 {
 							floating_bit_indices.push(i);
 						}
 					}
 
-					// let result = (value & !mask.0) | (mask.0 & mask.1);
-
-					let p = create_yes(floating_bit_indices.len() as u32);
-
-					for c in p {
-						// println!("{:?}", c);
-						let mut local_address = address;
-						for (i, &bit) in c.iter().enumerate() {
-							if bit == 1 {
-								local_address |= 1 << floating_bit_indices[i];
-							} else {
-								local_address &= !(1 << floating_bit_indices[i]);
+					let num_floating_bits = floating_bit_indices.len();
+					// All possible ways to set the floating bits is 2^n where n is number
+					// of floating bits
+					for n in 0..2i64.pow(num_floating_bits as u32) {
+						// Make copy of address for each iteration
+						let mut address = address;
+						for i in 0..num_floating_bits {
+							// Bit is one, since all floating bits in address are intialised
+							// to 0's we only need to upgrade from 0's to 1's
+							if n & 1 << i != 0 {
+								address |= 1 << floating_bit_indices[i];
 							}
 						}
-						// println!("changed {:b} to {:b}", address, local_address);
-						mem.insert(local_address, *value);
-						println!("wrinting {} to {}", value, local_address);
+
+						mem.insert(address, value);
 					}
 				}
 			}
@@ -66,22 +68,47 @@ impl Solver for Day14Solver {
 	}
 }
 
-enum Instruction {
-	Mask(i64, i64),
-	Mem(i64, i64),
+#[derive(Clone, Copy, Default)]
+struct SetMaskArgs {
+	mask_bits: i64,
+	override_bits: i64,
 }
 
-fn parse(input: &str) -> Vec<Instruction> {
+struct WriteArgs {
+	address: i64,
+	value: i64,
+}
+
+enum Instruction {
+	SetMask(SetMaskArgs),
+	Write(WriteArgs),
+}
+
+fn instructions(input: &str) -> Vec<Instruction> {
 	input
 		.lines()
 		.map(|line| {
 			if let Some(mask) = line.strip_prefix("mask = ") {
-				let mask_bits = mask.replace("0", "1").replace("X", "0");
-				let mask_override_bits = mask.replace("X", "0");
-				Instruction::Mask(
-					i64::from_str_radix(&mask_bits, 2).unwrap(),
-					i64::from_str_radix(&mask_override_bits, 2).unwrap(),
-				)
+				let mut mask_bits = 0;
+				for &c in mask.as_bytes() {
+					mask_bits <<= 1;
+					match c {
+						b'0' | b'1' => mask_bits |= 1,
+						_ => (),
+					}
+				}
+				let mut override_bits = 0;
+				for &c in mask.as_bytes() {
+					override_bits <<= 1;
+					match c {
+						b'1' => override_bits |= 1,
+						_ => (),
+					}
+				}
+				Instruction::SetMask(SetMaskArgs {
+					mask_bits,
+					override_bits,
+				})
 			} else {
 				let mut split = line.split("] = ");
 				let address = split
@@ -92,26 +119,10 @@ fn parse(input: &str) -> Vec<Instruction> {
 					.parse()
 					.unwrap();
 				let value = split.next().unwrap().parse().unwrap();
-				Instruction::Mem(address, value)
+				Instruction::Write(WriteArgs { address, value })
 			}
 		})
 		.collect()
-}
-
-fn create_yes(n: u32) -> Vec<Vec<i64>> {
-	let mut v = Vec::new();
-	for i in 0..(2i64.pow(n)) {
-		let mut c = Vec::new();
-		for j in 0..n {
-			if i & 1 << j != 0 {
-				c.push(1);
-			} else {
-				c.push(0);
-			}
-		}
-		v.push(c);
-	}
-	v
 }
 
 #[cfg(test)]
@@ -135,9 +146,9 @@ mod tests {
 	}
 
 	#[bench]
-	fn bench_parse(bencher: &mut Bencher) {
+	fn bench_parse_instructions(bencher: &mut Bencher) {
 		let input = fetch_input(14);
-		bencher.iter(|| parse(&input));
+		bencher.iter(|| instructions(&input));
 	}
 
 	#[bench]
