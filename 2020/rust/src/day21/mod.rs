53cd267fc9db271303635fe46bf0752839f38a05
Rasmus Rosengren <rasmus.rosengren@pm.me>
2020-12-21 17:10:35 +0100

    Day 21 clean up

--- 2020/rust/src/day21/mod.rs
+++ 2020/rust/src/day21/mod.rs
@@ -6,61 +6,14 @@ pub struct Day21Solver;
 
 impl Solver for Day21Solver {
 	fn solve_part_one(&self, input: &str) -> i64 {
-		let input = parse(input);
-		let mut allergents_set = HashSet::new();
-		for (_, allergents) in input.iter() {
-			for allergent in allergents.iter() {
-				allergents_set.insert(allergent);
-			}
-		}
-
-		println!("{:?}", allergents_set);
-		let mut ingredient_to_allergent = HashMap::new();
-
-		while !allergents_set.is_empty() {
-			let mut found = None;
-			for allergent in allergents_set.iter() {
-				let mut s = HashSet::new();
-				let mut first = true;
-				for (ingredients, allergents) in input.iter() {
-					if allergents.contains(allergent) {
-						if first {
-							first = false;
-							for ingredient in ingredients.iter() {
-								if !ingredient_to_allergent.contains_key(ingredient) {
-									s.insert(ingredient);
-								}
-							}
-						} else {
-							s.retain(|ing| ingredients.contains(ing));
-						}
-					}
-				}
-
-				println!("Remaining to deduce {}: {:?}", allergent, s);
-
-				if s.len() == 1 {
-					found = Some((allergent.to_owned(), s.iter().next().unwrap().to_owned()));
-					break;
-				}
-			}
-
-			if let Some((allergent, ingredient)) = found {
-				ingredient_to_allergent.insert(ingredient, allergent);
-				allergents_set.remove(allergent);
-			} else {
-				panic!(format!("{}", allergents_set.len()))
-			}
-		}
-
-		println!("{:?}", allergents_set);
-		println!("{:?}", ingredient_to_allergent);
+		let foods = parse_foods(input);
+		let ingredient_allergent_pairs = find_ingredient_allergent_pairs(&foods);
 
 		let mut count = 0;
 
-		for (ingredients, _) in input.iter() {
-			for ingredient in ingredients.iter() {
-				if !ingredient_to_allergent.contains_key(ingredient) {
+		for (ingredients, _) in foods.into_iter() {
+			for ingredient in ingredients.into_iter() {
+				if !ingredient_allergent_pairs.contains_key(ingredient) {
 					count += 1;
 				}
 			}
@@ -70,72 +23,25 @@ impl Solver for Day21Solver {
 	}
 
 	fn solve_part_two(&self, input: &str) -> i64 {
-		let input = parse(input);
-		let mut allergents_set = HashSet::new();
-		for (_, allergents) in input.iter() {
-			for allergent in allergents.iter() {
-				allergents_set.insert(allergent);
-			}
-		}
+		let foods = parse_foods(input);
+		let ingredient_to_allergent = find_ingredient_allergent_pairs(&foods);
 
-		println!("{:?}", allergents_set);
-		let mut ingredient_to_allergent = HashMap::new();
-
-		while !allergents_set.is_empty() {
-			let mut found = None;
-			for allergent in allergents_set.iter() {
-				let mut s = HashSet::new();
-				let mut first = true;
-				for (ingredients, allergents) in input.iter() {
-					if allergents.contains(allergent) {
-						if first {
-							first = false;
-							for ingredient in ingredients.iter() {
-								if !ingredient_to_allergent.contains_key(ingredient) {
-									s.insert(ingredient);
-								}
-							}
-						} else {
-							s.retain(|ing| ingredients.contains(ing));
-						}
-					}
-				}
-
-				println!("Remaining to deduce {}: {:?}", allergent, s);
-
-				if s.len() == 1 {
-					found = Some((allergent.to_owned(), s.iter().next().unwrap().to_owned()));
-					break;
-				}
-			}
-
-			if let Some((allergent, ingredient)) = found {
-				ingredient_to_allergent.insert(ingredient, allergent);
-				allergents_set.remove(allergent);
-			} else {
-				panic!(format!("{}", allergents_set.len()))
-			}
-		}
-
-		println!("{:?}", allergents_set);
-		println!("{:?}", ingredient_to_allergent);
-
-		let mut dangerous_ingredients: Vec<_> = ingredient_to_allergent.iter().collect();
+		let mut dangerous_ingredients: Vec<_> = ingredient_to_allergent.into_iter().collect();
 		dangerous_ingredients.sort_unstable_by(|left, right| left.1.cmp(&right.1));
 		println!(
 			"{:?}",
 			dangerous_ingredients
 				.iter()
-				.map(|(i, _)| ***i)
+				.map(|&(i, _)| i)
 				.collect::<Vec<_>>()
 				.join(",")
 		);
 
-		1
+		0
 	}
 }
 
-fn parse(input: &str) -> Vec<(Vec<&str>, Vec<&str>)> {
+fn parse_foods(input: &str) -> Vec<(HashSet<&str>, HashSet<&str>)> {
 	input
 		.lines()
 		.map(|line| {
@@ -150,6 +56,62 @@ fn parse(input: &str) -> Vec<(Vec<&str>, Vec<&str>)> {
 			(ingredients, allergents)
 		})
 		.collect()
+}
+
+fn find_ingredient_allergent_pairs<'a>(
+	foods: &Vec<(HashSet<&'a str>, HashSet<&'a str>)>,
+) -> HashMap<&'a str, &'a str> {
+	let mut all_allergents: HashSet<&str> = HashSet::new();
+	for (_, allergents) in foods.into_iter() {
+		for &allergent in allergents.iter() {
+			all_allergents.insert(allergent);
+		}
+	}
+
+	let mut ingredient_to_allergent: HashMap<&str, &str> = HashMap::new();
+	while !all_allergents.is_empty() {
+		let mut found = None;
+		for allergent in all_allergents.iter() {
+			// Ingredients that are all foods that contain the allergent, after checking
+			// all foods this hopefully leaves only one ingredient
+			let mut matching_ingredients: HashSet<&str> = HashSet::new();
+			// Since the matching_ingredients set is empty from the beginning, we need
+			// to fill it up with the first matching food's ingredients, and then reduce
+			let mut first_matching_food = true;
+			for (ingredients, allergents) in foods.into_iter() {
+				if allergents.contains(allergent) {
+					if first_matching_food {
+						first_matching_food = false;
+						for ingredient in ingredients.iter() {
+							if !ingredient_to_allergent.contains_key(ingredient) {
+								matching_ingredients.insert(ingredient);
+							}
+						}
+					} else {
+						matching_ingredients.retain(|ingredient| ingredients.contains(ingredient));
+					}
+				}
+			}
+
+			// If there is only one we have found a ingredient -> allergent pair
+			if matching_ingredients.len() == 1 {
+				found = Some((
+					allergent.to_owned(),
+					matching_ingredients.iter().next().unwrap().to_owned(),
+				));
+				break;
+			}
+		}
+
+		if let Some((allergent, ingredient)) = found {
+			ingredient_to_allergent.insert(ingredient, allergent);
+			all_allergents.remove(allergent);
+		} else {
+			panic!("No allergent could be matched to one specific ingredient");
+		}
+	}
+
+	ingredient_to_allergent
 }
 
 #[cfg(test)]
@@ -172,23 +134,23 @@ mod tests {
 		assert_eq!(solver.solve_part_two(input), 0);
 	}
 
-	// #[bench]
-	// fn bench_parse(bencher: &mut Bencher) {
-	// 	let input = fetch_input(21);
-	// 	bencher.iter(|| parse(&input));
-	// }
+	#[bench]
+	fn bench_parse_foods(bencher: &mut Bencher) {
+		let input = fetch_input(21);
+		bencher.iter(|| parse_foods(&input));
+	}
 
-	// #[bench]
-	// fn bench_part_one(bencher: &mut Bencher) {
-	// 	let input = fetch_input(21);
-	// 	let solver = Day21Solver {};
-	// 	bencher.iter(|| solver.solve_part_one(&input));
-	// }
+	#[bench]
+	fn bench_part_one(bencher: &mut Bencher) {
+		let input = fetch_input(21);
+		let solver = Day21Solver {};
+		bencher.iter(|| solver.solve_part_one(&input));
+	}
 
-	// #[bench]
-	// fn bench_part_two(bencher: &mut Bencher) {
-	// 	let input = fetch_input(21);
-	// 	let solver = Day21Solver {};
-	// 	bencher.iter(|| solver.solve_part_two(&input));
-	// }
+	#[bench]
+	fn bench_part_two(bencher: &mut Bencher) {
+		let input = fetch_input(21);
+		let solver = Day21Solver {};
+		bencher.iter(|| solver.solve_part_two(&input));
+	}
 }
