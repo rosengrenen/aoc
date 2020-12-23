2b91a83c9d8c489e7df97eb1cb9a7bd5ae2d5253
Rasmus Rosengren <rasmus.rosengren@pm.me>
2020-12-23 21:00:33 +0100

    Day 23 this wasn't much better lmao

--- 2020/rust/src/day23/mod.rs
+++ 2020/rust/src/day23/mod.rs
@@ -126,60 +126,88 @@ impl Solver for Day23Solver {
 		// numbers.sort_unstable();
 
 		// println!("{:?}", input);
-		let mut current = input[0];
 		let mut current_index = 0;
 
-		for m in 0..10_000_000 {
+		for m in 0..100 {
+			let current = input[current_index];
+
 			if m % 1000 == 0 {
 				println!("\n-- move {} --", m + 1);
 			}
 			// print!("cups: ");
 			// for cup in input.iter() {
 			// 	if *cup == current {
-			// 		print!("({}) ", cup);
+			// 		print!("({})", cup);
 			// 	} else {
-			// 		print!("{} ", cup);
+			// 		print!(" {} ", cup);
 			// 	}
 			// }
 			// print!("\n");
 
-			let mut a = input[(current_index + 1) % input.len()];
-			input.remove((current_index + 1) % input.len());
-			let mut b = input[(current_index + 1) % input.len()];
-			let mut c = input[(current_index + 1) % input.len()];
-			let mut new_current = input[(current_index + 4) % input.len()];
-			// TODO remove els and adjust index
+			let a = input[(current_index + 1) % input.len()];
+			let b = input[(current_index + 2) % input.len()];
+			let c = input[(current_index + 3) % input.len()];
 
-			// println!("pick up: {}, {}, {}", a, b, c);
+			// println!("pick up: {}, {}, {}", pick[0], pick[1], pick[2]);
 			let mut destination = current;
 			loop {
-				if destination == 0 {
-					destination = 1_000_000;
+				if destination == 1 {
+					destination = 10_000_000;
 				} else {
 					destination -= 1;
 				}
 
-				// println!("{}, {} {} {}", numbers[i], a, b, c);
-
-				if c != a && c != b && c != c {
+				if destination != a && destination != b && destination != c {
 					break;
 				}
 			}
 
 			// println!("destination: {}", destination);
-			let mut i = 0;
-			loop {
-				if input[i] == destination {
-					input.insert(i + 1, a);
-					input.insert(i + 2, b);
-					input.insert(i + 3, c);
-					break;
+			let input_len = input.len();
+			let mut from_index = (current_index + 4) % input_len;
+			let mut to_index = (current_index + 1) % input_len;
+			while to_index != current_index {
+				if input[from_index] == destination {
+					input[to_index] = input[from_index];
+					input[(to_index + 1) % input_len] = a;
+					input[(to_index + 2) % input_len] = b;
+					input[(to_index + 3) % input_len] = c;
+					to_index = (to_index + 4) % input_len;
+					from_index = (from_index + 1) % input_len;
+				} else {
+					input[to_index] = input[from_index];
+					to_index = (to_index + 1) % input_len;
+					from_index = (from_index + 1) % input_len;
 				}
-				i += 1;
 			}
+
+			// for i in 1..input.len() - 3 {
+			// 	let offset = if placed { 3 } else { 0 };
+			// 	let from_index = (current_index + i + 3 + offset) % input_len;
+			// 	let to_index = (current_index + i + offset) % input_len;
+			// 	if destination == input[from_index] {
+			// 		placed = true;
+			// 		println!("{}", input[from_index]);
+			// 		println!("{}", pick[0]);
+			// 		println!("{}", pick[1]);
+			// 		println!("{}", pick[2]);
+			// 	} else {
+			// 		println!("{}", input[from_index]);
+			// 	}
+			// 	// input[(current_index + i) % input_len] = input[(current_index + i + 3) % input_len];
+			// }
+			// let mut i = 0;
+			// loop {
+			// 	if input[i] == destination {
+			// 		input.insert(i + 1, a);
+			// 		input.insert(i + 2, b);
+			// 		input.insert(i + 3, c);
+			// 		break;
+			// 	}
+			// 	i += 1;
+			// }
 			// println!("{:?}", input);
-			current = new_current;
-			current_index = new_current_index;
+			current_index = (current_index + 1) % input.len();
 			// let mut i = 0;
 			// loop {
 			// 	if input[i] == current {
