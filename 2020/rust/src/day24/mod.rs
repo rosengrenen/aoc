08924a2132714abcff3ff7b81cdf5e482bbdf9d8
Rasmus Rosengren <rasmus.rosengren@pm.me>
2020-12-24 07:57:39 +0100

    Day 24 cleanup 2/2

--- 2020/rust/src/day24/mod.rs
+++ 2020/rust/src/day24/mod.rs
@@ -7,153 +7,19 @@ pub struct Day24Solver;
 impl Solver for Day24Solver {
 	fn solve_part_one(&self, input: &str) -> i64 {
 		let instructions = parse_instructions(input);
-		let mut tiles = HashSet::new();
-		for instruction in instructions.iter() {
-			let mut pos = (0, 0);
-			for &direction in instruction.iter() {
-				match direction {
-					Dir::E => pos = (pos.0 + 1, pos.1),
-					Dir::NE => {
-						if pos.1 % 2 == 0 {
-							pos = (pos.0 + 1, pos.1 + 1)
-						} else {
-							pos = (pos.0, pos.1 + 1)
-						}
-					}
-					Dir::NW => {
-						if pos.1 % 2 == 0 {
-							pos = (pos.0, pos.1 + 1)
-						} else {
-							pos = (pos.0 - 1, pos.1 + 1)
-						}
-					}
-					Dir::SE => {
-						if pos.1 % 2 == 0 {
-							pos = (pos.0 + 1, pos.1 - 1)
-						} else {
-							pos = (pos.0, pos.1 - 1)
-						}
-					}
-					Dir::SW => {
-						if pos.1 % 2 == 0 {
-							pos = (pos.0, pos.1 - 1)
-						} else {
-							pos = (pos.0 - 1, pos.1 - 1)
-						}
-					}
-					Dir::W => pos = (pos.0 - 1, pos.1),
-				}
-			}
-
-			if tiles.contains(&pos) {
-				tiles.remove(&pos);
-			} else {
-				tiles.insert(pos);
-			}
-		}
-
-		tiles.len() as i64
+		let tiles = tiles_from_instructions(&instructions);
+		tiles.len() as i64 + 1
 	}
 
 	fn solve_part_two(&self, input: &str) -> i64 {
-		let directions_list = parse_instructions(input);
-		let mut tiles = HashSet::new();
-		for directions in directions_list.iter() {
-			let mut pos = (0, 0);
-			for &direction in directions.iter() {
-				match direction {
-					Dir::E => pos = (pos.0 + 1, pos.1),
-					Dir::NE => {
-						if pos.1 % 2 == 0 {
-							pos = (pos.0 + 1, pos.1 + 1)
-						} else {
-							pos = (pos.0, pos.1 + 1)
-						}
-					}
-					Dir::NW => {
-						if pos.1 % 2 == 0 {
-							pos = (pos.0, pos.1 + 1)
-						} else {
-							pos = (pos.0 - 1, pos.1 + 1)
-						}
-					}
-					Dir::SE => {
-						if pos.1 % 2 == 0 {
-							pos = (pos.0 + 1, pos.1 - 1)
-						} else {
-							pos = (pos.0, pos.1 - 1)
-						}
-					}
-					Dir::SW => {
-						if pos.1 % 2 == 0 {
-							pos = (pos.0, pos.1 - 1)
-						} else {
-							pos = (pos.0 - 1, pos.1 - 1)
-						}
-					}
-					Dir::W => pos = (pos.0 - 1, pos.1),
-				}
-			}
-
-			if tiles.contains(&pos) {
-				tiles.remove(&pos);
-			} else {
-				tiles.insert(pos);
-			}
-		}
-
-		let even_neighbours = [(0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, 0)];
-		let odd_neighbours = [(-1, 1), (0, 1), (1, 0), (0, -1), (-1, -1), (-1, 0)];
-
+		let instructions = parse_instructions(input);
+		let mut tiles = tiles_from_instructions(&instructions);
 		for _ in 0..100 {
-			let mut new_tiles = HashSet::new();
-			for &(x, y) in tiles.iter() {
-				let neighbours = if y % 2 == 0 {
-					even_neighbours
-				} else {
-					odd_neighbours
-				};
-				for &(dx, dy) in neighbours.iter() {
-					let ax = x + dx;
-					let ay = y + dy;
-					let count = count_neighbours(&tiles, (ax, ay));
-
-					if tiles.contains(&(ax, ay)) {
-						if count == 1 || count == 2 {
-							new_tiles.insert((ax, ay));
-						}
-					} else {
-						if count == 2 {
-							new_tiles.insert((ax, ay));
-						}
-					}
-				}
-			}
-			tiles = new_tiles;
+			tiles = simulate(&tiles);
 		}
 
 		tiles.len() as i64
 	}
-}
-
-fn count_neighbours(tiles: &HashSet<(i64, i64)>, pos: (i64, i64)) -> i64 {
-	let (x, y) = pos;
-	let even_neighbours = [(0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, 0)];
-	let odd_neighbours = [(-1, 1), (0, 1), (1, 0), (0, -1), (-1, -1), (-1, 0)];
-
-	let neightbours = if y % 2 == 0 {
-		even_neighbours
-	} else {
-		odd_neighbours
-	};
-	let mut count = 0;
-	for (dx, dy) in neightbours.iter() {
-		if tiles.contains(&(x + dx, y + dy)) {
-			count += 1;
-		}
-	}
-
-	count
 }
 
 #[derive(Copy, Clone, Debug)]
@@ -234,7 +100,7 @@ fn tiles_from_instructions(instructions: &Vec<Vec<Dir>>) -> HashSet<Pos> {
 					pos.y += 1;
 				}
 				Dir::NW => {
-					if pos.y % 2 == 1 {
+					if pos.y % 2 != 0 {
 						pos.x -= 1;
 					}
 					pos.y += 1;
@@ -246,7 +112,7 @@ fn tiles_from_instructions(instructions: &Vec<Vec<Dir>>) -> HashSet<Pos> {
 					pos.y -= 1;
 				}
 				Dir::SW => {
-					if pos.y % 2 == 1 {
+					if pos.y % 2 != 0 {
 						pos.x -= 1;
 					}
 					pos.y -= 1;
@@ -263,6 +129,53 @@ fn tiles_from_instructions(instructions: &Vec<Vec<Dir>>) -> HashSet<Pos> {
 	}
 
 	tiles
+}
+
+static EVEN_NEIGHBOURS: [(i64, i64); 6] = [(0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, 0)];
+static ODD_NEIGHBOURS: [(i64, i64); 6] = [(-1, 1), (0, 1), (1, 0), (0, -1), (-1, -1), (-1, 0)];
+
+fn count_neighbours(tiles: &HashSet<Pos>, pos: Pos) -> i64 {
+	let neightbours = if pos.y % 2 == 0 {
+		EVEN_NEIGHBOURS
+	} else {
+		ODD_NEIGHBOURS
+	};
+	let mut count = 0;
+	for (dx, dy) in neightbours.iter() {
+		let cur_pos = Pos::new(pos.x + dx, pos.y + dy);
+		if tiles.contains(&cur_pos) {
+			count += 1;
+		}
+	}
+
+	count
+}
+
+fn simulate(tiles: &HashSet<Pos>) -> HashSet<Pos> {
+	let mut new_tiles = HashSet::new();
+	for &Pos { x, y } in tiles.iter() {
+		let neighbours = if y % 2 == 0 {
+			EVEN_NEIGHBOURS
+		} else {
+			ODD_NEIGHBOURS
+		};
+		for &(dx, dy) in neighbours.iter() {
+			let cur_pos = Pos::new(x + dx, y + dy);
+			let count = count_neighbours(&tiles, cur_pos);
+
+			if tiles.contains(&cur_pos) {
+				if count == 1 || count == 2 {
+					new_tiles.insert(cur_pos);
+				}
+			} else {
+				if count == 2 {
+					new_tiles.insert(cur_pos);
+				}
+			}
+		}
+	}
+
+	new_tiles
 }
 
 #[cfg(test)]
@@ -285,23 +198,23 @@ mod tests {
 		assert_eq!(solver.solve_part_two(input), 2208);
 	}
 
-	// #[bench]
-	// fn bench_parse_cups(bencher: &mut Bencher) {
-	// 	let input = fetch_input(24);
-	// 	bencher.iter(|| parse(&input));
-	// }
+	#[bench]
+	fn bench_parse_instructions(bencher: &mut Bencher) {
+		let input = fetch_input(24);
+		bencher.iter(|| parse_instructions(&input));
+	}
 
-	// #[bench]
-	// fn bench_part_one(bencher: &mut Bencher) {
-	// 	let input = fetch_input(24);
-	// 	let solver = Day24Solver {};
-	// 	bencher.iter(|| solver.solve_part_one(&input));
-	// }
+	#[bench]
+	fn bench_part_one(bencher: &mut Bencher) {
+		let input = fetch_input(24);
+		let solver = Day24Solver {};
+		bencher.iter(|| solver.solve_part_one(&input));
+	}
 
-	// #[bench]
-	// fn bench_part_two(bencher: &mut Bencher) {
-	// 	let input = fetch_input(24);
-	// 	let solver = Day24Solver {};
-	// 	bencher.iter(|| solver.solve_part_two(&input));
-	// }
+	#[bench]
+	fn bench_part_two(bencher: &mut Bencher) {
+		let input = fetch_input(24);
+		let solver = Day24Solver {};
+		bencher.iter(|| solver.solve_part_two(&input));
+	}
 }
