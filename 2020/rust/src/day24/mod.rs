9e3fb333a571e42ea92e7b3caa71f7c7d4755a1d
Rasmus Rosengren <rasmus.rosengren@pm.me>
2020-12-25 06:35:40 +0100

    Fix day 24 test :P

--- 2020/rust/src/day24/mod.rs
+++ 2020/rust/src/day24/mod.rs
@@ -8,7 +8,7 @@ impl Solver for Day24Solver {
 	fn solve_part_one(&self, input: &str) -> i64 {
 		let instructions = parse_instructions(input);
 		let tiles = tiles_from_instructions(&instructions);
-		tiles.len() as i64 + 1
+		tiles.len() as i64
 	}
 
 	fn solve_part_two(&self, input: &str) -> i64 {
