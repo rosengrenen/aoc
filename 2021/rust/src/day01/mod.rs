48ddf84a94d920e28d51fe98d820c931fb488308
Rasmus Rosengren <rasmus.rosengren@protonmail.com>
2021-12-01 11:45:18 +0100

    2021 rust day 01

--- 2021/rust/src/day01/mod.rs
+++ 2021/rust/src/day01/mod.rs
@@ -4,11 +4,42 @@ use aoc_util::{Solver, SolverOutput};
 pub struct Day1;
 
 impl Solver for Day1 {
-	fn part_one(&self, _input: &str) -> SolverOutput {
-		SolverOutput::Num(0)
+	fn part_one(&self, input: &str) -> SolverOutput {
+		let depths = input.lines().map(|line| line.parse::<i64>().unwrap());
+
+		let mut times_increased = 0;
+		let mut prev_depth = None;
+		for depth in depths {
+			if let Some(prev_depth) = prev_depth {
+				if depth > prev_depth {
+					times_increased += 1;
+				}
+			}
+
+			prev_depth = Some(depth);
+		}
+
+		SolverOutput::Num(times_increased)
 	}
 
-	fn part_two(&self, _input: &str) -> SolverOutput {
-		SolverOutput::Num(0)
+	fn part_two(&self, input: &str) -> SolverOutput {
+		let depths: Vec<i64> = input.lines().map(|line| line.parse().unwrap()).collect();
+
+		let mut times_increased = 0;
+		let mut prev_window = None;
+		for window_end in 3..=depths.len() {
+			let window_start = window_end - 3;
+			let window: i64 = depths[window_start..window_end].iter().sum();
+
+			if let Some(prev_sum) = prev_window {
+				if window > prev_sum {
+					times_increased += 1;
+				}
+			}
+
+			prev_window = Some(window);
+		}
+
+		SolverOutput::Num(times_increased)
 	}
 }
