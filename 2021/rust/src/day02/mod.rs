ef639cc00de21eca704542b3c74de3ece80adbf6
Rasmus Rosengren <rasmus.rosengren@protonmail.com>
2021-12-02 10:14:28 +0100

    2021 rust day 02

---
+++ 2021/rust/src/day02/mod.rs
@@ -0,0 +1,62 @@
+use aoc_util::{Solver, SolverOutput};
+
+#[derive(Default)]
+pub struct Day2;
+
+impl Solver for Day2 {
+	fn part_one(&self, input: &str) -> SolverOutput {
+		let commands = parse_commands(input);
+		let mut h_pos = 0;
+		let mut v_pos = 0;
+		for (dir, amount) in commands {
+			match dir {
+				Direction::Forward => h_pos += amount,
+				Direction::Down => v_pos += amount,
+				Direction::Up => v_pos -= amount,
+			}
+		}
+
+		SolverOutput::Num(h_pos * v_pos)
+	}
+
+	fn part_two(&self, input: &str) -> SolverOutput {
+		let commands = parse_commands(input);
+		let mut h_pos = 0;
+		let mut v_pos = 0;
+		let mut aim = 0;
+		for (dir, amount) in commands {
+			match dir {
+				Direction::Forward => {
+					h_pos += amount;
+					v_pos += aim * amount;
+				}
+				Direction::Down => aim += amount,
+				Direction::Up => aim -= amount,
+			}
+		}
+
+		SolverOutput::Num(h_pos * v_pos)
+	}
+}
+
+enum Direction {
+	Forward,
+	Down,
+	Up,
+}
+
+fn parse_commands(input: &str) -> Vec<(Direction, i64)> {
+	input
+		.lines()
+		.map(|line| {
+			let (dir, amount) = line.split_once(' ').unwrap();
+			let dir = match dir {
+				"forward" => Direction::Forward,
+				"down" => Direction::Down,
+				"up" => Direction::Up,
+				_ => panic!("Unknown direction {}", dir),
+			};
+			(dir, amount.parse().unwrap())
+		})
+		.collect()
+}
