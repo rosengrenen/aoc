d911d2ef20c2d531389c221d03e2b6643da8240e
Rasmus Rosengren <rasmus.rosengren@protonmail.com>
2022-12-01 12:52:44 +0100

    simplify python

--- 2022/python/day01/solver.py
+++ 2022/python/day01/solver.py
@@ -1,12 +1,6 @@
-def solve01_p1(input):
-    invs = sorted([sum([int(item) for item in inv.splitlines()])
-                   for inv in input.split('\n\n')], reverse=True)
+input = open("input.txt").read()
+invs = sorted([sum([int(item) for item in inv.splitlines()])
+               for inv in input.split('\n\n')], reverse=True)
 
-    return invs[0] + invs[1] + invs[2]
-
-
-def solve01_p2(input):
-    invs = sorted([sum([int(item) for item in inv.splitlines()])
-                   for inv in input.split('\n\n')], reverse=True)
-
-    return invs[0]
+print("Part 1:", invs[0])
+print("Part 2:", invs[0] + invs[1] + invs[2])
