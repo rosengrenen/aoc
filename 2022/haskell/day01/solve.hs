422a3388ba489476e41d4a69433ce79324d4e7b4
Rasmus Rosengren <rasmus.rosengren@protonmail.com>
2022-12-01 15:13:29 +0100

    haskell day01

---
+++ 2022/haskell/day01/solve.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Data.List (sort)
+import qualified Data.Text as T
+
+splitOn :: String -> String -> [String]
+splitOn delim input = map T.unpack $ T.splitOn (T.pack delim) (T.pack input)
+
+parseInput :: String -> [Int]
+parseInput input = map (sum . map read . splitOn "\n") (splitOn "\n\n" input)
+
+solvePart1 :: String -> Int
+solvePart1 = maximum . parseInput
+
+solvePart2 :: String -> Int
+solvePart2 = sum . take 3 . reverse . sort . parseInput
+
+main :: IO ()
+main =
+  do
+    input <- readFile "input.txt"
+    putStrLn $ "Part one: " ++ show (solvePart1 input)
+    putStrLn $ "Part two: " ++ show (solvePart2 input)
+    return ()
