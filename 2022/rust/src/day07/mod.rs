ca165d9595895294880897bfc67e2d7f118a375d
Rasmus Rosengren <rasmus.rosengren@protonmail.com>
2022-12-07 09:55:17 +0100

    day07 rust

---
+++ 2022/rust/src/day07/mod.rs
@@ -0,0 +1,111 @@
+use std::collections::HashMap;
+
+use aoc_util::{Solver, SolverOutput};
+
+#[derive(Default)]
+pub struct Day7;
+
+impl Solver for Day7 {
+  fn part_one(&self, input: &str) -> SolverOutput {
+    let dir = parse_commands(input);
+    SolverOutput::Num(dirs_with_size(&dir))
+  }
+
+  fn part_two(&self, input: &str) -> SolverOutput {
+    let dir = parse_commands(input);
+    let free_space = 70000000 - dir.size();
+    let space_to_free = 30000000 - free_space;
+    SolverOutput::Num(dir_to_delete(&dir, space_to_free).unwrap().size())
+  }
+}
+
+fn dirs_with_size(dir: &Directory) -> i64 {
+  let dir_size = dir.size();
+  let mut dir_size = if dir_size <= 100000 { dir_size } else { 0 };
+
+  for dir in dir.dirs.values() {
+    dir_size += dirs_with_size(&dir);
+  }
+
+  dir_size
+}
+
+fn dir_to_delete<'a>(dir: &'a Directory<'a>, space_to_free: i64) -> Option<&'a Directory<'a>> {
+  if dir.size() < space_to_free {
+    return None;
+  }
+
+  let mut dirs = dir.dirs.values().collect::<Vec<_>>();
+  dirs.sort_unstable_by(|l, r| l.size().cmp(&r.size()));
+  for dir in dirs {
+    if dir.size() >= space_to_free {
+      if let Some(dir) = dir_to_delete(dir, space_to_free) {
+        return Some(dir);
+      } else {
+        return Some(dir);
+      }
+    }
+  }
+
+  return Some(dir);
+}
+
+fn parse_commands(input: &str) -> Directory {
+  let mut root_dir = Directory::default();
+  let mut current_path = vec![];
+  for cmd in input.split("$") {
+    if cmd.trim().starts_with("cd") {
+      let parts = cmd.trim().split_ascii_whitespace().collect::<Vec<_>>();
+      let to = parts[1];
+      if to.starts_with("/") {
+        current_path = to.split("/").filter(|part| !part.is_empty()).collect();
+      } else if to == ".." {
+        current_path.pop();
+      } else {
+        current_path.push(to)
+      }
+    } else {
+      let files = cmd
+        .trim()
+        .lines()
+        .skip(1)
+        .filter_map(|line| {
+          let (size, _) = line.split_once(" ").unwrap();
+          if size == "dir" {
+            None
+          } else {
+            Some(size.parse().unwrap())
+          }
+        })
+        .collect::<Vec<_>>();
+      root_dir.set_files(&current_path, files);
+    }
+  }
+
+  root_dir
+}
+
+#[derive(Debug, Default)]
+struct Directory<'a> {
+  files: Vec<i64>,
+  dirs: HashMap<&'a str, Directory<'a>>,
+}
+
+impl<'a> Directory<'a> {
+  fn set_files(&mut self, path: &[&'a str], files: Vec<i64>) {
+    if path.is_empty() {
+      self.files = files;
+    } else {
+      let dir = self.dirs.entry(path[0]).or_default();
+      dir.set_files(&path[1..], files);
+    }
+  }
+
+  fn size(&self) -> i64 {
+    let files_size = self.files.iter().sum();
+    self
+      .dirs
+      .values()
+      .fold(files_size, |total, dir| total + dir.size())
+  }
+}
