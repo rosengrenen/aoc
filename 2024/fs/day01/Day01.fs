a15c64e6dc834c20cbbf36f41bf86e3f726bbd6c
Rasmus Rosengren <rasmus.rosengren@protonmail.com>
2024-12-01 09:47:31 +0100

    2024 fsharp seq

--- 2024/fs/day01/Day01.fs
+++ 2024/fs/day01/Day01.fs
@@ -10,26 +10,26 @@ let parseLine (line: string) =
 
 let parse (input: string) =
     let lines = lines input |> Seq.map parseLine
-    let leftList = Seq.map (fun parts -> fst parts) lines |> Seq.toList
-    let rightList = Seq.map (fun parts -> snd parts) lines |> Seq.toList
-    List.sort leftList, List.sort rightList
+    let left = lines |> Seq.map fst
+    let right = lines |> Seq.map snd
+    Seq.sort left, Seq.sort right
 
 let part1 input =
     let left, right = parse input
 
-    List.zip left right
-    |> List.map (fun (left, right) -> left - right |> abs)
-    |> List.sum
+    Seq.zip left right
+    |> Seq.map (fun (left, right) -> left - right |> abs)
+    |> Seq.sum
     |> string
 
 let part2 input =
     let left, right = parse input
-    let frequency = List.countBy id right |> Map
+    let frequency = right |> Seq.countBy id |> Map
 
     let getFrequency value =
         frequency |> Map.tryFind value |> Option.defaultValue 0
 
-    left |> List.map (fun value -> value * getFrequency value) |> List.sum |> string
+    left |> Seq.map (fun value -> value * getFrequency value) |> Seq.sum |> string
 
 let part1Tests =
     testList "Day 1 part 1 tests" [ partTestCase part1 "day01/example1.txt" "11" ]
